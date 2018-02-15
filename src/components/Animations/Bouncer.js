e9c6552ec814c4ea57b286b362e4518dbf278e66
Diego Landa <diegolanda@msn.com>
2018-02-15 17:56:02 -0400

    Added Bouncer component

---
+++ src/components/Animations/Bouncer.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+import './Bouncer.css';
+
+class Bouncer extends PureComponent {
+  render() {
+    return (
+      <div className="wui-bouncer">
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default Bouncer;
---
+++ src/components/Animations/__tests__/Bouncer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Bouncer from '../Bouncer';
+
+import { mount } from 'enzyme';
+
+describe('ImageTag Component', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Bouncer/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should render with children', () => {
+    const children = <span>Bouncer</span>;
+
+    const wrapper = mount(<Bouncer>{children}</Bouncer>);
+
+    const domNode = wrapper.find(Bouncer).getDOMNode();
+
+    expect(domNode.className).toBe('wui-bouncer');
+    expect(wrapper.find(Bouncer).props().children).toBe(children);
+  })
+});
