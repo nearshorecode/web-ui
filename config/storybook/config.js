0914f0d119523595c36130559a695c4b8a77ed54
Wilber <hd.wilber@gmail.com>
2018-02-15 16:47:34 -0400

    Updated storybook with info addon

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,8 +1,18 @@
 import { configure } from '@storybook/react';
+import { setDefaults } from '@storybook/addon-info';
 
 function loadStories() {
   require('../../src/stories/index.js');
 }
+
+// addon-info
+setDefaults({
+  header: false, 
+  inline: true,
+  maxPropsIntoLine: 1,
+  maxPropStringLength: 200,
+});
+
 
 configure(loadStories, module);
 
