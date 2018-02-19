0de8002df273e7228dcb06d01631f97709464720
raeioul <raeioul@gmail.com>
2018-02-19 18:40:47 -0400

    Added style prop to tech-icon and update the test. (#16)
    Added style props to TechIcon component
    
    * Added style prop to tech-icon and update the test.
    
    * Added 2 Test on TechIcon to check style prop
    
    * Fix suggestions on tests. Change names on constants to Uppercase.

--- src/components/__tests__/TechIcon.js
+++ src/components/__tests__/TechIcon.js
@@ -6,11 +6,16 @@ import { mount } from 'enzyme';
 
 const DEFAULT_PROPS = {
   name: 'android',
-  width:'78px', 
-  height:'78px'
 };
 
 const IMAGE_PATH = './assets/tech-icons/';
+const STYLE = {
+  padding: "20px",
+}; 
+const SIZE = {
+  width: "20px",
+  height: "20px",
+}; 
 
 describe('Icon Component', () => {
   it('renders without crashing', () => {
@@ -28,6 +33,28 @@ describe('Icon Component', () => {
      expect(props.alt).toBe(`${IMAGE_PATH}${DEFAULT_PROPS.name}.png`);
   })
   
+  it('Should add style prop when passed', () => {
+    const wrapper = mount(<TechIcon name="android" style={STYLE} />);
+
+    const props = wrapper.find('img').props();
+
+     expect(props.src).toBe(`${DEFAULT_PROPS.name}.png`);
+     expect(props.alt).toBe(`${IMAGE_PATH}${DEFAULT_PROPS.name}.png`);
+     expect(props.style.padding).toBe(`${STYLE.padding}`);
+  })
+
+  it('should override style width and height if size and style props are present', () => {
+    const wrapper = mount(<TechIcon name="android" style={STYLE} size={SIZE}/>);
+
+    const props = wrapper.find('img').props();
+
+     expect(props.src).toBe(`${DEFAULT_PROPS.name}.png`);
+     expect(props.alt).toBe(`${IMAGE_PATH}${DEFAULT_PROPS.name}.png`);
+     expect(props.style.padding).toBe(`${STYLE.padding}`);
+     expect(props.style.width).toBe(`${SIZE.width}`);
+     expect(props.style.height).toBe(`${SIZE.height}`);
+  })
+
   it('contains only one img', () => {
     const wrapper = mount(<TechIcon {...DEFAULT_PROPS}/>);
 
