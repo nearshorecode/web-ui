cbe716fb25577a2bd350ec1cc9159a09dcca2607
Raúl Acuña <raeioul@gmail.com>
2018-02-20 11:15:54 -0400

    Fix the style bug with TechIconList and add unit test.

--- src/components/__tests__/TechIconList.js
+++ src/components/__tests__/TechIconList.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import TechIconList from '../TechIconList';
 import TechIcon from '../TechIcon';
-
 import { mount } from 'enzyme';
 
 const DEFAULT_PROPS = {
@@ -13,7 +11,13 @@ const DEFAULT_PROPS = {
   ],
   flow: 'horizontal',
 };
-
+const STYLE_PROPS = {
+  icons: [
+    {name:'android', width:'18px', height:'78px',
+      style:{padding:'50px',}
+    }
+  ]  
+};
 describe('Icon Component', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -25,9 +29,13 @@ describe('Icon Component', () => {
     const wrapper = mount(
       <TechIconList {...DEFAULT_PROPS}/>
     );
-
     const children = wrapper.find(TechIconList).find(TechIcon);
-
     expect(children.length).toBe(2);
+  })
+  
+  it('Should add style prop when passed', () => {
+    const wrapper = mount(<TechIconList {...STYLE_PROPS} />);
+    const props = wrapper.find('img').props();
+    expect(props.style.padding).toBe(`${STYLE_PROPS.icons[0].style.padding}`);
   })
 });
