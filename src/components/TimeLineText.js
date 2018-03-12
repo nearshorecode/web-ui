4a6ee2d13e921947db10fa482b790605c4ed0340
raeioul <raeioul@raeioul.192.168.0.119>
2018-03-12 18:36:07 -0400

    Cutting the icon from the component.

--- src/components/TimeLineText.js
+++ src/components/TimeLineText.js
@@ -1,32 +1,29 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import 'font-awesome/css/font-awesome.min.css';
-import './TimeLine.css';
+import './TimeLineText.css';
 
 export const INIT = {
   left: 'left',
   right: 'right',
 };
 
-class TimeLine extends PureComponent {
+class TimeLineText extends PureComponent {
   getInit = () => {
     const { init } = this.props;
     return init === INIT.left ? 'row' : 'row-reverse';
   };
   render() {
     const init = this.getInit();
-    const { icon, title, text, style } = this.props;
+    const { title, text } = this.props;
     return (
       <div>
         <div className="wui-timeline-container" style={{ flexDirection: init }}>
           <span>
             <strong>{title}</strong>
           </span>
-          <span className="wui-timeline-container-hr">
+          <span className="wui-hr">
             <hr />
-          </span>
-          <span>
-            <i className={icon} style={style} />
           </span>
         </div>
         <div className="wui-timeline-container" style={{ flexDirection: init }}>
@@ -36,7 +33,7 @@ class TimeLine extends PureComponent {
     );
   }
 }
-TimeLine.propTypes = {
+TimeLineText.propTypes = {
   icon: PropTypes.string.isRequired,
   title: PropTypes.string,
   text: PropTypes.string,
@@ -44,9 +41,9 @@ TimeLine.propTypes = {
   style: PropTypes.object,
 };
 
-TimeLine.defaultProps = {
+TimeLineText.defaultProps = {
   init: 'left',
   style: {},
 };
 
-export default TimeLine;
+export default TimeLineText;
